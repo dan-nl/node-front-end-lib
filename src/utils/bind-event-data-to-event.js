82fc3783a95113f05d42fa71ef55ec95d4f1d070
dan-nl <d_entous@yahoo.com>
2016-06-23 16:31:20 +0200

    added type checking to bindEventDataToEvent

--- src/utils/bind-event-data-to-event.js
+++ src/utils/bind-event-data-to-event.js
@@ -6,6 +6,25 @@
  * @param {Function} fn
  * @param {Object} event_data
  */module.exports = function bindEventDataToEvent( elm, event_name, fn, event_data ) {
+  if ( !elm ) {
+    console.warn( 'bindEventDataToEvent(): elm not provided as an element' );
+    return;
+  }
+
+  if ( typeof event_name !== 'string' ) {
+    console.warn( 'bindEventDataToEvent(): event_name not provided as a string' );
+    return;
+  }
+
+  if ( !( fn instanceof Function ) ) {
+    console.warn( 'bindEventDataToEvent(): fn not provided as an Object' );
+    return;
+  }
+
+  if ( !( event_data instanceof Object ) ) {
+    event_data = {};
+  }
+
   elm.addEventListener(
     event_name,
     function bindEventData( evt ) {
