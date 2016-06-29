12ab8eba8563bd0b5308bbb964d411f4da62029f
dan-nl <d_entous@yahoo.com>
2016-06-29 12:19:51 +0200

    added use of getComputedStyle to getElementHeight when available

--- src/dom/get-element-height.js
+++ src/dom/get-element-height.js
@@ -11,19 +11,32 @@ var getBoundingClientRect;
 getBoundingClientRect = require( './get-bounding-client-rect' );
 
 /**
- * returns the height, in pixels, of the elm provided. remember to add the string 'px' to the value returned when using it in a `elm.style.x =` statement
- *
  * @param {HTMLElement} elm
- * @returns {number|undefined}
+ * @returns {number}
  */
-module.exports = function getElementHeight( elm ) {
-  var bounding_client_rect;
+function useComputedStyle( elm ) {
+  var computed_style;
+  var result;
 
-  // validations
-  if ( !elm || elm.constructor.toString().indexOf( 'HTML' ) === -1 ) {
-    console.warn( 'getElementHeight( ' + elm + ' ): elm not provided as an HTMLElement' );
-    return;
-  }
+  computed_style = window.getComputedStyle( elm, null );
+
+  result = parseInt( computed_style.getPropertyValue( 'margin-top' ), 10 );
+  result += parseInt( computed_style.getPropertyValue( 'border-top-width' ), 10 );
+  result += parseInt( computed_style.getPropertyValue( 'padding-top' ), 10 );
+  result += parseInt( computed_style.getPropertyValue( 'height' ), 10 );
+  result += parseInt( computed_style.getPropertyValue( 'padding-bottom' ), 10 );
+  result += parseInt( computed_style.getPropertyValue( 'border-bottom-width' ), 10 );
+  result += parseInt( computed_style.getPropertyValue( 'margin-bottom' ), 10 );
+
+  return result;
+}
+
+/**
+ * @param {HTMLElement} elm
+ * @returns {number}
+ */
+function useBoundingClientRect( elm ) {
+  var bounding_client_rect;
 
   bounding_client_rect = getBoundingClientRect( elm );
 
@@ -34,4 +47,28 @@ module.exports = function getElementHeight( elm ) {
       elm.scrollHeight
     )
   );
+}
+
+/**
+ * returns the height, in pixels, of the elm provided. remember to add the string 'px' to the value returned when using it in a `elm.style.x =` statement
+ *
+ * @param {HTMLElement} elm
+ * @returns {number}
+ */
+module.exports = function getElementHeight( elm ) {
+  var error;
+
+  // validations
+  if ( !elm || elm.constructor.toString().indexOf( 'HTML' ) === -1 ) {
+    console.warn( 'getElementHeight( ' + elm + ' ): elm not provided as an HTMLElement' );
+    error = new Error( 'stack trace' );
+    console.warn( error.stack );
+    return 0;
+  }
+
+  if ( !( window.getComputedStyle instanceof Function ) ) {
+    return useBoundingClientRect( elm );
+  }
+
+  return useComputedStyle( elm );
 };
